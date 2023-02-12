65d9d07134b44dbfce7d4f5984a9f5563280b1b7
Daniel <danielahumada071@gmail.com>
2023-02-12 12:16:46 -0500

    agregamos funcionalidad al boton

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 console.log("funciona")
+
+const boton_microfono = document.getElementById("btn_microfono")
+
+boton_microfono.addEventListener("click", mensaje)
+
+function mensaje() {
+    console.log("microfono");
+}
+
